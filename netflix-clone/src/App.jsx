de3827b75c6d0a79b6e9b72af4b1cc6d99d560aa
solarios777 <tsegasolomon635@gmail.com>
2024-07-04 10:51:47 +0300

    header

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>sol</h1>
-    </>
+    <div className="App">
+      <Home />
+    </div>
   );
 }
 
---
+++ netflix-clone/src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./header.css";
+import Netflixlogo from "../../assets/images/netflix.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className="nav-left">
+          <a href="#" className="logo">
+            <img src={Netflixlogo} alt="Netflix Logo" />
+          </a>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">TV Shows</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">latest</a>
+            </li>
+            <li>
+              <a href="#">My List</a>
+            </li>
+            <li>
+              <a href="#">Browse by languages</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-right">
+          <a href="#" className="search-icon">
+            <SearchIcon />
+          </a>
+          <a href="#" className="notifications-icon">
+            <NotificationsNoneIcon />
+          </a>
+
+          <div className="user-profile">
+            <a href="#" className="account-box-icon">
+              <AccountBoxIcon />
+              <a href="#" className="account-box-icon">
+                <KeyboardArrowDownIcon />
+              </a>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
